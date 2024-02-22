1e6d99144f34fe8996c0193598edca933eb62fc2
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-02-22 10:53:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
+const sbmt = document.querySelector("#submit");
+      sbmt.addEventListener('click', volume_sphere);
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+      function volume_sphere(){
+
+          var radius = document.getElementById("radius").value;
+          console.log(`Rr-adius: ${radius}` );
+        
+         if (isNaN(radius)) {
+           alert("Please enter a valid number for the radius.");
+           return;
+          }
+    
+        // Calculate the Volume of the sphere
+        var vol = 4 * Math.PI * Math.pow(radius, 3)/3;
+    
+        // Display the result into volume box
+        var resultElement = document.getElementById("volume");
+        resultElement.value =  vol.toFixed(2); 
+	  }
+
+//window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
